a6380f44c5707fb9e1e175349acc49f45f46c267
alhassane-coder <74630964+alhassane-coder@users.noreply.github.com>
2021-04-19 15:10:05 +0000

    Delete qrcode.php

--- qrcode.php
+++
@@ -1,9 +0,0 @@
-<?php
-// On utilise la libraie phpqrcode pour générer nos codes QR
-require_once('librairies/phpqrcode/qrlib.php');
-
-//Le contenu de notre  code sera le lien vers le produit
-$data='http://pfe.rtest.local/Pfe/productView.phpid=1';
-
-//On génère enfin le code
-QRcode::png($data,2);
